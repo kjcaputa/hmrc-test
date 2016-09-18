a6d5e0d62a37484e5fd48d3bfa64e0f99975f44d
Kris <kjcaputa@gmail.com>
2016-09-18 20:00:10 +0100

    HTKJC-ST01-001: Test added for check if checkout ststem return an empty list when bask has been empty

--- src/main/scala/net/atos/kjc/fruitshop/checkoutsystem/Checkout.scala
+++ src/main/scala/net/atos/kjc/fruitshop/checkoutsystem/Checkout.scala
@@ -2,4 +2,7 @@ package net.atos.kjc.fruitshop.checkoutsystem
 
 object Checkout {
   def name = "Eco Fruit shop checkout system"
+
+  def makePurchase(items:List[String]) = ???
+
 }
--- src/test/scala/net/atos/kjc/fruitshop/checkoutsystem/Checkout.scala
+++ src/test/scala/net/atos/kjc/fruitshop/checkoutsystem/Checkout.scala
@@ -1,13 +1,20 @@
 package net.atos.kjc.fruitshop.checkoutsystem
 
 import org.scalatest.{FreeSpec, Matchers}
+import Matchers._
 
 class CheckoutSpec extends FreeSpec with Matchers {
   val checkoutName = "Eco Fruit shop checkout system"
 
+  val basketEmpty: List[String] = List()
+
   "Checkout system should" - {
     "Present right name" - {
       Checkout.name shouldBe checkoutName
+    }
+
+    "Return an empty purchase for empty list of products" -{
+      assert(Checkout.makePurchase(basketEmpty).equals(Nil))
     }
   }
 }
