25f0b50539ad2440068a349c2dd5200b80388297
Kris <kjcaputa@gmail.com>
2016-09-18 20:37:05 +0100

    HTKJC-ST01-004: Test added for a Purchases bagging area for non empty basket

--- src/test/scala/net/atos/kjc/fruitshop/checkoutsystem/Checkout.scala
+++ src/test/scala/net/atos/kjc/fruitshop/checkoutsystem/Checkout.scala
@@ -7,7 +7,13 @@ import net.atos.kjc.fruitshop.purchase.Purchase
 class CheckoutSpec extends FreeSpec with Matchers {
   val checkoutName = "Eco Fruit shop checkout system"
 
+  val apple = "Apple"
+
   val basketEmpty: List[String] = List()
+
+  val basket1Apple:List[String] = List(apple)
+  val basket2Apples:List[String] = basket1Apple :+ apple
+  val basket3Apples:List[String] = basket2Apples :+ apple
 
   "Checkout system should" - {
     "Present right name" - {
@@ -19,8 +25,17 @@ class CheckoutSpec extends FreeSpec with Matchers {
         Checkout.makePurchase(basketEmpty) shouldBe a[Purchase]
       }
       "Return empty bagging area" - {
-        assert(Checkout.makePurchase(basketEmpty).baggingArea.equals(Nil))
-        //        Checkout.makePurchase(basketEmpty).baggingArea shouldBe Nil
+        Checkout.makePurchase(basketEmpty).baggingArea shouldBe Nil
+      }
+
+      "The bagging area should contain one Apple" - {
+        Checkout.makePurchase(basket1Apple).baggingArea.length shouldBe 1
+      }
+      "The bagging area should contain two Apples" - {
+        Checkout.makePurchase(basket2Apples).baggingArea.length shouldBe 2
+      }
+      "The bagging area should contain tree Apples" - {
+        Checkout.makePurchase(basket3Apples).baggingArea.length shouldBe 3
       }
     }
 
