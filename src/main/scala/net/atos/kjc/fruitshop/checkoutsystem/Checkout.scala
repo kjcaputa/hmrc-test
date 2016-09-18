e86b69b24afc1a30f0c7a5568b86652160f00107
Kris <kjcaputa@gmail.com>
2016-09-18 20:09:53 +0100

    HTKJC-ST01-002: Fix code

--- src/main/scala/net/atos/kjc/fruitshop/checkoutsystem/Checkout.scala
+++ src/main/scala/net/atos/kjc/fruitshop/checkoutsystem/Checkout.scala
@@ -5,6 +5,6 @@ import net.atos.kjc.fruitshop.purchase.Purchase
 object Checkout {
   def name = "Eco Fruit shop checkout system"
 
-  def makePurchase(items:List[String]):Purchase = ???
+  def makePurchase(items:List[String]):Purchase = Purchase()
 
 }
