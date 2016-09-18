6093b36322f089810e9ce8adfe69e936f27e4ebd
Kris <kjcaputa@gmail.com>
2016-09-18 21:09:05 +0100

    HTKJC-ST01-005: Fix code to pass the test

--- src/main/scala/net/atos/kjc/fruitshop/purchase/Purchase.scala
+++ src/main/scala/net/atos/kjc/fruitshop/purchase/Purchase.scala
@@ -1,5 +1,25 @@
 package net.atos.kjc.fruitshop.purchase
 
-case class Purchase(items: List[String]){
-  val baggingArea = items
+case class Purchase(items: List[String]) {
+  val baggingArea = {
+
+    def productChecker(item: String): Boolean = item match {
+      case "Apple" => true
+      case "Orange" => true
+      case _ => false
+    }
+
+    def helper(items: List[String], acc: List[String]): List[String] = {
+      items match {
+        case Nil => acc
+        case x :: xs =>
+          if (productChecker(x))
+            helper(xs, acc :+ x)
+          else
+            helper(xs, acc)
+      }
+    }
+
+    helper(items, Nil)
+  }
 }
