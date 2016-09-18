f93ce50f49f32dcc608ae063de57920c63fa2119
Kris <kjcaputa@gmail.com>
2016-09-18 23:52:32 +0100

    HTKJC-ST02-014: Test added for check of applying offers to a purchase

--- src/test/scala/net/atos/kjc/fruitshop/checkoutsystem/Checkout.scala
+++ src/test/scala/net/atos/kjc/fruitshop/checkoutsystem/Checkout.scala
@@ -10,5 +10,19 @@ class CheckoutSpec extends FreeSpec with Matchers with TestConstanceSpec{
       Checkout.name shouldBe checkoutName
     }
 
+    "offers" - {
+      "one Apple" - {
+        val purchase = Checkout.makePurchase(basket1Apple)
+        assert(purchase.getTotalCost === 0.60)
+      }
+      "two Apple" - {
+        val purchase = Checkout.makePurchase(basket2Apples)
+        assert(purchase.getTotalCost === 0.60)
+      }
+      "tree Apple" - {
+        val purchase = Checkout.makePurchase(basket3Apples)
+        assert(purchase.getTotalCost === 0.60 * 2)
+      }
+    }
   }
 }
