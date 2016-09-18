03696cf01c4a692a111d5b9f67f9d185afffc85b
Kris <kjcaputa@gmail.com>
2016-09-19 00:21:56 +0100

    HTKJC-ST02-014: correct tests. Implement offers into checkout system

--- src/main/scala/net/atos/kjc/fruitshop/checkoutsystem/Checkout.scala
+++ src/main/scala/net/atos/kjc/fruitshop/checkoutsystem/Checkout.scala
@@ -1,10 +1,16 @@
 package net.atos.kjc.fruitshop.checkoutsystem
 
+import net.atos.kjc.fruitshop.items.{Apples, Oranges}
+import net.atos.kjc.fruitshop.offers.Offer
 import net.atos.kjc.fruitshop.purchase.Purchase
 
 object Checkout {
   def name = "Eco Fruit shop checkout system"
 
-  def makePurchase(items:List[String]):Purchase = Purchase(items)
+  def makePurchase(items: List[String]): Purchase = Purchase(items, offers)
 
+  val offers: List[Offer] = List(
+    Offer(2, 1, Apples(1)),
+    Offer(3, 2, Oranges(1))
+  )
 }
--- src/main/scala/net/atos/kjc/fruitshop/purchase/Purchase.scala
+++ src/main/scala/net/atos/kjc/fruitshop/purchase/Purchase.scala
@@ -1,8 +1,9 @@
 package net.atos.kjc.fruitshop.purchase
 
 import net.atos.kjc.fruitshop.items.{Apples, InvalidProducts, Items, Oranges}
+import net.atos.kjc.fruitshop.offers.Offer
 
-case class Purchase(items: List[String]) {
+case class Purchase(items: List[String], offers: List[Offer] = Nil) {
   val baggingArea: List[Items] = {
 
     def productChecker(item: String): Items = item match {
@@ -31,7 +32,16 @@ case class Purchase(items: List[String]) {
 
   lazy val getPurchaseLength: Int = baggingArea.foldLeft(0)(_ + _.amount)
 
-  lazy val getTotalCost: Double = baggingArea.foldLeft(0.0)((acc, i) => acc + (i.cost * i.amount))
+  lazy val getTotalCost: Double = baggingArea.foldLeft(0.0) { (acc, i) =>
+    offers.find(_.item.getClass.getCanonicalName == i.getClass.getCanonicalName) match {
+      case Some(x) =>
+        val m = i.amount % x.amount
+        acc + (i.cost * x.priceInRelationToUnitCost * (i.amount - m) / x.amount) + (i.cost * m)
+      case x =>
+        acc + (i.cost * i.amount)
+    }
+  }
+
 
   lazy val getSummary: String =
     baggingArea.map(item => "%dx %s".format(item.amount, item.name)).mkString("[", ", ", "] = £%2.2f".format(getTotalCost))
